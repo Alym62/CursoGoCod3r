321cc16335cb2cecc796bc94155103caa66eec2c
Alym62 <gasafm62@gmail.com>
2024-07-21 17:40:40 -0300

    feat: first classroom.

---
+++ fundamentals/first/first.go
@@ -0,0 +1,13 @@
+// Executable programs start with the main package
+package main
+
+/*
+Codes in Go are organized into packages
+and to use them you need to declare them in import
+*/
+import "fmt"
+
+// The input to a Go program is the main function
+func main() {
+	fmt.Println("Hello World!")
+}
