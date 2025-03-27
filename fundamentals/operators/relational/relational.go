dd508a4069eaf5e26dfdcba5d3cc830f47cbd0df
Alyasaf Meireles <gasafm62@gmail.com>
2025-03-27 17:26:35 -0300

    feat: class on about operators relational

---
+++ fundamentals/operators/relational/relational.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	Name string
+}
+
+func main() {
+	fmt.Println("== :", "Banana" == "Banana")
+	fmt.Println("!= :", 3 != 2)
+	fmt.Println("< :", 3 < 2)
+	fmt.Println("> :", 3 > 2)
+	fmt.Println("<= :", 3 <= 2)
+	fmt.Println(">= :", 3 >= 2)
+
+	dateOne := time.Unix(0, 0)
+	dateTwo := time.Unix(0, 0)
+
+	fmt.Println("data without pointer:", dateOne == dateTwo)
+	fmt.Println("data with 'equals':", dateOne.Equal(dateTwo))
+
+	personOne := Person{Name: "João"}
+	personTwo := Person{Name: "João"}
+
+	fmt.Println("Person:", personOne == personTwo)
+}
