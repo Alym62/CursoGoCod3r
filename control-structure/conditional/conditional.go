6e978c4f3dceee422fb66e6081a33c675248fc70
Alyasaf Meireles <gasafm62@gmail.com>
2025-03-28 13:42:55 -0300

    feat: second class on about structure conditional with if and else

--- control-structure/conditional/conditional.go
+++ control-structure/conditional/conditional.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func noteToConcept(note float64) string {
+	if note >= 9 && note <= 10 {
+		return "A"
+	} else if note >= 8 && note < 9 {
+		return "B"
+	} else if note >= 5 && note < 8 {
+		return "C"
+	} else {
+		return "D"
+	}
+}
+
 func result(note float64) {
 	if note >= 7 {
 		fmt.Println("Aprovado com nota:", note)
@@ -13,4 +25,7 @@ func result(note float64) {
 func main() {
 	result(0.1)
 	result(7.1)
+
+	concept := noteToConcept(9.8)
+	fmt.Println(concept)
 }
